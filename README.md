36797f0f493c68202cc4023cf7c9c7d09e902300
AQP20171679 <alejandraadriantejada@gmail.com>
2017-07-13 17:13:03 -0500

    Codigo funciona #falta readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # lista-frutas
+## Pasos
+1 
+2
+3 En el HTML
+  * Titulo: Frutas Master con tamaño de titilo <h1>
+  * Comentario: **Frutas Master** es una empresa familiar de tercera generación que...
+  * Insertar imagen (en este caso un gif)
+  
---
+++ assets/js/javascript.js
@@ -0,0 +1,13 @@
+var frutas=["Manzana", "Pera", "Piña", "Cereza","Naranja"];
+
+function agregarFruta(){
+	var frutaIngresada=document.getElementById('ingresoFruta').value;
+	var respuestaFrutal = document.getElementById('frutaNueva');
+	
+	var muchasFrutas="";
+	frutas.push(frutaIngresada);
+	for (i=5; i<frutas.length; i++){
+		muchasFrutas+=i+1+". "+frutas[i]+"<br>";
+	}
+	respuestaFrutal.innerHTML=muchasFrutas;
+}
